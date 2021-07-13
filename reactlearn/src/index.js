35ca68ab13aa2aff84336ff93d4f3acd27bc9abd
Chandra Shekhar <shekhar88085@gmail.com>
2021-07-14 02:00:57 +0530

    Declared a winner

--- reactlearn/src/index.js
+++ reactlearn/src/index.js
@@ -24,7 +24,7 @@ class Square extends React.Component {
     }
   }
 
-  
+
   
   class Board extends React.Component {
 
@@ -32,14 +32,18 @@ class Square extends React.Component {
         super(props);
           this.state={
             squares:Array(9).fill(null),
+            xIsNext:true,
           };
       }
 
 handleClick(i){
   const squares=
   this.state.squares.slice();
-  squares[i]='X';
-this.setState({squares:squares});
+  squares[i]=this.state.xIsNext?'X':'O';
+this.setState({
+  squares:squares,
+  xIsNext: !this.state.xIsNext,
+});
 }
 
       renderSquare(i) {
@@ -49,9 +53,15 @@ this.setState({squares:squares});
             onClick={() => this.handleClick(i)}      />
         );
       }
-  
-    render() {
-      const status = 'Next player: X';
+
+      render() {
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+          status = 'Winner: ' + winner;
+        } else {
+          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
   
       return (
         <div>
@@ -99,3 +109,24 @@ this.setState({squares:squares});
     document.getElementById('root')
   );
   
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
